2b236529c67d4f1db56969a2b863df08598a21df
semendolgix75 <semen.dolgix@yandex.ru>
2024-06-20 19:47:21 +0500

    Ready HomeWork

--- src/main/java/pro/sky/course2/HomeWork24/controller/BasketController.java
+++ src/main/java/pro/sky/course2/HomeWork24/controller/BasketController.java
@@ -1,1 +1,28 @@
 package pro.sky.course2.HomeWork24.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.course2.HomeWork24.service.BasketService;
+
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+@RequestMapping("/store/order")
+public class BasketController {
+    @Autowired
+    private BasketService basketService;
+
+    @GetMapping("/add")
+    public List<Integer> add(@RequestParam List<Integer> items) {
+        return basketService.add(items);
+    }
+
+    @GetMapping
+    public List<Integer> get() {
+        return basketService.get();
+    }
+}
--- src/main/java/pro/sky/course2/HomeWork24/service/BasketServiceImpl.java
+++ src/main/java/pro/sky/course2/HomeWork24/service/BasketServiceImpl.java
@@ -1,2 +1,30 @@
 package pro.sky.course2.HomeWork24.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+import pro.sky.course2.HomeWork24.model.Basket;
+
+import java.util.Collection;
+import java.util.List;
+@SessionScope
+@Service
+public class BasketServiceImpl implements BasketService {
+
+    private final Basket basket;
+    public BasketServiceImpl(Basket basket) {
+        this.basket = basket;
+    }
+
+    @Override
+    public List<Integer> add(List<Integer> items) {
+        for (Integer id : items)
+            basket.addProduct(id);
+        return items;
+    }
+
+    @Override
+    public List<Integer> get() {
+        return basket.get();
+
+    }
+}
